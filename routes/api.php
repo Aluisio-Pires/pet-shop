ff0cf9e3e9148585fb0e61758e9c42a81244c668
Aluisio Pires <aluisiopireseng@gmail.com>
2022-02-17 20:48:36 -0300

    ApiRoutes configurated with apiResources

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\FileController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderStatusController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PromotionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +25,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+    'prefix' => 'v1',
+], function (){
+    Route::apiResources([
+        'brands' => BrandController::class,
+        'categories' => CategoryController::class,
+        'files' => FileController::class,
+        'posts' => PostController::class,
+        'products' => ProductController::class,
+        'promotions' => PromotionController::class,
+        'orders' => OrderController::class,
+        'order_statuses' => OrderStatusController::class,
+        'payments' => PaymentController::class,
+    ]);
 });
