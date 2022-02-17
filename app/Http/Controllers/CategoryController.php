87957b1af584de8463afe8c9fa94bb63ca081b59
Aluisio Pires <aluisiopireseng@gmail.com>
2022-02-17 20:53:50 -0300

    updating Category Controller and requests

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,17 +5,35 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Requests\UpdateCategoryRequest;
+use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return string
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $order='asc';
+        $column ='created_at';
+
+        if ($request->desc === 'true'){
+            $order='desc';
+        }
+        if ($request->has('sortBy')){
+            $column = $request->sortBy;
+        }
+        $model = Category::orderBy($column, $order);
+
+        if ($request->has('limit')){
+            $model = $model->take($request->limit)->get();
+        }else{
+            $model = $model->paginate();
+        }
+
+        return response()->json($model, 200);
     }
 
     /**
@@ -32,22 +50,28 @@ class CategoryController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\StoreCategoryRequest  $request
-     * @return \Illuminate\Http\Response
+     * @return Category
      */
     public function store(StoreCategoryRequest $request)
     {
-        //
+        $category = new Category([
+            'title' => $request->title,
+            'slug' => $request->slug,
+        ]);
+        $category->save();
+
+        return $category;
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
+     * @return Category
      */
     public function show(Category $category)
     {
-        //
+        return $category;
     }
 
     /**
@@ -66,21 +90,26 @@ class CategoryController extends Controller
      *
      * @param  \App\Http\Requests\UpdateCategoryRequest  $request
      * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
+     * @return Category
      */
     public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+        $category->update([
+            'title' => $request->title,
+            'slug' => $request->slug,
+        ]);
+        return $category;
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return response()->json(['OK'], 200);
     }
 }
