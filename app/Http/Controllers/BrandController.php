c202534f44eaf8f06455f7aaf2338c67fed391f1
Aluisio Pires <aluisiopireseng@gmail.com>
2022-02-17 20:51:18 -0300

    updating Brand Controller and requests

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -5,17 +5,35 @@ namespace App\Http\Controllers;
 use App\Models\Brand;
 use App\Http\Requests\StoreBrandRequest;
 use App\Http\Requests\UpdateBrandRequest;
+use Illuminate\Http\Request;
 
 class BrandController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return string
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $order='asc';
+        $column ='created_at';
+
+        if ($request->desc === 'true'){
+            $order='desc';
+        }
+        if ($request->has('sortBy')){
+            $column = $request->sortBy;
+        }
+        $model = Brand::orderBy($column, $order);
+
+        if ($request->has('limit')){
+            $model = $model->take($request->limit)->get();
+        }else{
+            $model = $model->paginate();
+        }
+
+        return response()->json($model, 200);
     }
 
     /**
@@ -32,22 +50,29 @@ class BrandController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\StoreBrandRequest  $request
-     * @return \Illuminate\Http\Response
+     * @return Brand
      */
     public function store(StoreBrandRequest $request)
     {
-        //
+
+        $brand = new Brand([
+            'title' => $request->title,
+            'slug' => $request->slug,
+        ]);
+        $brand->save();
+
+        return $brand;
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\Brand  $brand
-     * @return \Illuminate\Http\Response
+     * @return Brand
      */
     public function show(Brand $brand)
     {
-        //
+        return $brand;
     }
 
     /**
@@ -66,21 +91,26 @@ class BrandController extends Controller
      *
      * @param  \App\Http\Requests\UpdateBrandRequest  $request
      * @param  \App\Models\Brand  $brand
-     * @return \Illuminate\Http\Response
+     * @return Brand
      */
     public function update(UpdateBrandRequest $request, Brand $brand)
     {
-        //
+        $brand->update([
+            'title' => $request->title,
+            'slug' => $request->slug,
+        ]);
+        return $brand;
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Brand  $brand
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Brand $brand)
     {
-        //
+        $brand->delete();
+        return response()->json(['OK'], 200);
     }
 }
--- app/Http/Requests/StoreBrandRequest.php
+++ app/Http/Requests/StoreBrandRequest.php
@@ -13,7 +13,7 @@ class StoreBrandRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class StoreBrandRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required|string',
+            'slug' => 'required|string',
         ];
     }
 }
