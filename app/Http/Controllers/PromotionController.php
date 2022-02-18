e5e50bb0df022b1c541dae538636a56c12637546
Aluisio Pires <aluisiopireseng@gmail.com>
2022-02-17 21:09:01 -0300

    updating Promotion Controller and requests

--- app/Http/Controllers/PromotionController.php
+++ app/Http/Controllers/PromotionController.php
@@ -5,17 +5,35 @@ namespace App\Http\Controllers;
 use App\Models\Promotion;
 use App\Http\Requests\StorePromotionRequest;
 use App\Http\Requests\UpdatePromotionRequest;
+use Illuminate\Http\Request;
 
 class PromotionController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return string
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $order='asc';
+        $column ='created_at';
+
+        if ($request->desc === 'true'){
+            $order='desc';
+        }
+        if ($request->has('sortBy')){
+            $column = $request->sortBy;
+        }
+        $model = Promotion::orderBy($column, $order);
+
+        if ($request->has('limit')){
+            $model = $model->take($request->limit)->get();
+        }else{
+            $model = $model->paginate();
+        }
+
+        return response()->json($model, 200);
     }
 
     /**
@@ -32,22 +50,25 @@ class PromotionController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\StorePromotionRequest  $request
-     * @return \Illuminate\Http\Response
+     * @return Promotion
      */
     public function store(StorePromotionRequest $request)
     {
-        //
+        $promotion = new Promotion($request->all());
+        $promotion->save();
+
+        return $promotion;
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\Promotion  $promotion
-     * @return \Illuminate\Http\Response
+     * @return Promotion
      */
     public function show(Promotion $promotion)
     {
-        //
+        return $promotion;
     }
 
     /**
@@ -66,21 +87,23 @@ class PromotionController extends Controller
      *
      * @param  \App\Http\Requests\UpdatePromotionRequest  $request
      * @param  \App\Models\Promotion  $promotion
-     * @return \Illuminate\Http\Response
+     * @return Promotion
      */
     public function update(UpdatePromotionRequest $request, Promotion $promotion)
     {
-        //
+        $promotion->update($request->all());
+        return $promotion;
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Promotion  $promotion
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Promotion $promotion)
     {
-        //
+        $promotion->delete();
+        return response()->json(['OK'], 200);
     }
 }
