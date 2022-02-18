c5c3c3535b1ff44aad0feecbae80d9e6846055ff
Aluisio Pires <aluisiopireseng@gmail.com>
2022-02-17 21:05:15 -0300

    updating Payment Controller and requests

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -5,17 +5,35 @@ namespace App\Http\Controllers;
 use App\Models\Payment;
 use App\Http\Requests\StorePaymentRequest;
 use App\Http\Requests\UpdatePaymentRequest;
+use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return string
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $order='asc';
+        $column ='created_at';
+
+        if ($request->desc === 'true'){
+            $order='desc';
+        }
+        if ($request->has('sortBy')){
+            $column = $request->sortBy;
+        }
+        $model = Payment::orderBy($column, $order);
+
+        if ($request->has('limit')){
+            $model = $model->take($request->limit)->get();
+        }else{
+            $model = $model->paginate();
+        }
+
+        return response()->json($model, 200);
     }
 
     /**
@@ -32,22 +50,27 @@ class PaymentController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\StorePaymentRequest  $request
-     * @return \Illuminate\Http\Response
+     * @return Payment
      */
     public function store(StorePaymentRequest $request)
     {
-        //
+        $payment = new Payment([
+            'type' => $request->type,
+            'details' =>$request->details,
+        ]);
+        $payment->save();
+        return $payment;
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\Payment  $payment
-     * @return \Illuminate\Http\Response
+     * @return Payment
      */
     public function show(Payment $payment)
     {
-        //
+        return $payment;
     }
 
     /**
@@ -66,21 +89,26 @@ class PaymentController extends Controller
      *
      * @param  \App\Http\Requests\UpdatePaymentRequest  $request
      * @param  \App\Models\Payment  $payment
-     * @return \Illuminate\Http\Response
+     * @return Payment
      */
     public function update(UpdatePaymentRequest $request, Payment $payment)
     {
-        //
+        $payment->update([
+            'type' => $request->type,
+            'details' =>$request->details,
+        ]);
+        return $payment;
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Payment  $payment
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Payment $payment)
     {
-        //
+        $payment->delete();
+        return response()->json(['OK'], 200);
     }
 }
--- app/Http/Requests/StorePaymentRequest.php
+++ app/Http/Requests/StorePaymentRequest.php
@@ -13,7 +13,7 @@ class StorePaymentRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class StorePaymentRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'type' => 'required|string',
+            'details' => 'required|json',
         ];
     }
 }
