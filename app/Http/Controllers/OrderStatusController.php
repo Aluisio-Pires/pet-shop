e5ecb10892deaa851f06b9d48b6492c1b1a6ad27
Aluisio Pires <aluisiopireseng@gmail.com>
2022-02-17 21:04:13 -0300

    updating OrderStatus Controller and requests

--- app/Http/Controllers/OrderStatusController.php
+++ app/Http/Controllers/OrderStatusController.php
@@ -5,17 +5,35 @@ namespace App\Http\Controllers;
 use App\Models\OrderStatus;
 use App\Http\Requests\StoreOrderStatusRequest;
 use App\Http\Requests\UpdateOrderStatusRequest;
+use Illuminate\Http\Request;
 
 class OrderStatusController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return string
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $order='asc';
+        $column ='created_at';
+
+        if ($request->desc === 'true'){
+            $order='desc';
+        }
+        if ($request->has('sortBy')){
+            $column = $request->sortBy;
+        }
+        $model = OrderStatus::orderBy($column, $order);
+
+        if ($request->has('limit')){
+            $model = $model->take($request->limit)->get();
+        }else{
+            $model = $model->paginate();
+        }
+
+        return response()->json($model, 200);
     }
 
     /**
@@ -32,22 +50,26 @@ class OrderStatusController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\StoreOrderStatusRequest  $request
-     * @return \Illuminate\Http\Response
+     * @return OrderStatus
      */
     public function store(StoreOrderStatusRequest $request)
     {
-        //
+        $status = new OrderStatus([
+            'title' => $request->title,
+        ]);
+        $status->save();
+        return $status;
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\OrderStatus  $orderStatus
-     * @return \Illuminate\Http\Response
+     * @return OrderStatus
      */
     public function show(OrderStatus $orderStatus)
     {
-        //
+        return $orderStatus;
     }
 
     /**
@@ -66,21 +88,25 @@ class OrderStatusController extends Controller
      *
      * @param  \App\Http\Requests\UpdateOrderStatusRequest  $request
      * @param  \App\Models\OrderStatus  $orderStatus
-     * @return \Illuminate\Http\Response
+     * @return OrderStatus
      */
     public function update(UpdateOrderStatusRequest $request, OrderStatus $orderStatus)
     {
-        //
+        $orderStatus->update([
+            'title' => $request->title,
+        ]);
+        return $orderStatus;
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\OrderStatus  $orderStatus
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(OrderStatus $orderStatus)
     {
-        //
+        $orderStatus->delete();
+        return response()->json(['OK'], 200);
     }
 }
