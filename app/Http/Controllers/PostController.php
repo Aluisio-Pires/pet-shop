052ecd805c4375056d4970ada37d65bbb73002e0
Aluisio Pires <aluisiopireseng@gmail.com>
2022-02-17 21:06:21 -0300

    updating Post Controller and requests

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,17 +5,35 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return string
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $order='asc';
+        $column ='created_at';
+
+        if ($request->desc === 'true'){
+            $order='desc';
+        }
+        if ($request->has('sortBy')){
+            $column = $request->sortBy;
+        }
+        $model = Post::orderBy($column, $order);
+
+        if ($request->has('limit')){
+            $model = $model->take($request->limit)->get();
+        }else{
+            $model = $model->paginate();
+        }
+
+        return response()->json($model, 200);
     }
 
     /**
@@ -32,22 +50,25 @@ class PostController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\StorePostRequest  $request
-     * @return \Illuminate\Http\Response
+     * @return Post
      */
     public function store(StorePostRequest $request)
     {
-        //
+        $post = new Post($request->all());
+        $post->save();
+
+        return $post;
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
+     * @return Post
      */
     public function show(Post $post)
     {
-        //
+        return $post;
     }
 
     /**
@@ -66,21 +87,24 @@ class PostController extends Controller
      *
      * @param  \App\Http\Requests\UpdatePostRequest  $request
      * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
+     * @return Post
      */
     public function update(UpdatePostRequest $request, Post $post)
     {
-        //
+        $post->update($request->all());
+        return $post;
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return response()->json(['OK'], 200);
+
     }
 }
